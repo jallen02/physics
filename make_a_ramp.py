5f46daa46f70d1f95d28037552a58dcdc2aa62c2
Jason Allen <j.nella12@gmail.com>
2017-08-25 00:52:47 -0400

    ramp making ACTUALLY completed

--- make_a_ramp.py
+++ make_a_ramp.py
@@ -4,18 +4,44 @@ from pygame.locals import *
 import pymunk
 import pymunk.pygame_util
 
-height, width = 600
+height = 600
+width = 600
+ball_radius = 20
+ball_mass = 1
+gravity = (0.0, 900)
+starting_point = (50, 50)
+line_width = 1
+
+def add_ball(space, pos):
+    mass = ball_mass
+    radius = ball_radius
+    moment = pymunk.moment_for_circle(mass, 0, radius)
+    body = pymunk.Body(mass,moment)
+    body.position = pos
+    shape = pymunk.Circle(body, radius)
+    space.add(body, shape)
+    return shape
 
 def add_line(space, pos1, pos2):
-    vector = pos2 - pos1
+    vector = (pos2[0]-pos1[0], pos2[1]-pos1[1])
 
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
     body.position = pos1
-    line = pymunk.Segment(body, (
+    line = pymunk.Segment(body, (0,0), vector, line_width)
+    
+    space.add(line)
+    return line
+
+def draw_balls(screen, balls):
+    for ball in balls:
+        p = int(ball.body.position.x), int(ball.body.position.y)
+        pygame.draw.circle(screen, pygame.color.THECOLORS["blue"], p, ball_radius)
 
 def draw_lines(screen, lines):
     for line in lines:
-        pygame.draw.line(screen,pygame.color.THECOLORS["black"], 
+        p1 = line.body.position + line.a
+        p2 = line.body.position + line.b
+        pygame.draw.line(screen,pygame.color.THECOLORS["black"], p1, p2, 2)
 
 def main():
     pygame.init()
@@ -24,8 +50,10 @@ def main():
     clock = pygame.time.Clock()
 
     space = pymunk.Space()
-    space.gravity = (0.0, -900.0)
+    space.gravity = gravity
 
+    balls = []
+    lines = []
     draw_preview = False
 
     while True:
@@ -35,22 +63,35 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 sys.exit(0)
             elif event.type == MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                draw_preview = True;
+                pos1 = pygame.mouse.get_pos()
+                draw_preview = True
             elif event.type == MOUSEBUTTONUP:
                 pos2 = pygame.mouse.get_pos()
-                draw_preview = False;
+                lines.append(add_line(space, pos1, pos2))
+                draw_preview = False
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                balls.append(add_ball(space, starting_point))
 
         screen.fill((255,255,255))
         
-        pygame.draw.circle(screen, pygame.color.THECOLORS["blue"], (50,50), 20, 2)
+        pygame.draw.circle(screen, pygame.color.THECOLORS["blue"], starting_point, ball_radius, 2)
 
         if draw_preview:
             pygame.draw.line(screen, pygame.color.THECOLORS["lightgray"], \
-                             pos, pygame.mouse.get_pos())
+                             pos1, pygame.mouse.get_pos())
+
+        draw_lines(screen, lines)
+        draw_balls(screen, balls)
+        for ball in balls:
+            if ball.body.position.x > width + ball_radius or \
+                    ball.body.position.y > height + ball_radius:
+                space.remove(ball)
+                balls.remove(ball)
+
+        space.step(1/50.0)
 
         pygame.display.flip()
-
+        clock.tick(50)
 
 if __name__ == '__main__':
     sys.exit(main())
